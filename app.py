fb7f2acb4164d67c7d3202e4c917d3c7030fcb04
Matthijs Dingemanse <66972420+hierismatis@users.noreply.github.com>
2024-11-08 16:27:00 +0100

    testing

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ app = Flask(__name__)
 def return_contacts():
     return "<h1>He rens dikke sukkel</h1>"
 
-# @app.route('/convert', methods=['GET'])
-# def return_contacts():
-#     contacts = get_contacts()
-#     return contacts
+@app.route('/convert', methods=['GET'])
+def return_contacts():
+    contacts = get_contacts()
+    return contacts
 
 if __name__ == '__main__':
     app.run()
