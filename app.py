d85a274df8738f457177b0fc446e82d80e8a8f54
Matthijs Dingemanse <66972420+hierismatis@users.noreply.github.com>
2024-11-08 16:06:16 +0100

    fixed bug

--- app.py
+++ app.py
@@ -8,10 +8,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def return_contacts():
-    return "He rens dikke sukkel ga eens gdansk boeken"
-
-if __name__ == '__main__':
-    app.run()
+    return "<h1>He rens dikke sukkel</h1>"
 
 @app.route('/convert', methods=['GET'])
 def return_contacts():
