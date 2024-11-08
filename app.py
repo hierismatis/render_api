5901dce079ec705e6f407b3c18b35b3c65ad0356
Matthijs Dingemanse <66972420+hierismatis@users.noreply.github.com>
2024-11-08 17:09:50 +0100

    finished the contactdata per field point

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
 import pandas as pd
-from extract_data import get_contacts
+import extract_data
 import os
 
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
-def return_contacts():
+def home(): 
     return "<h1>He rens dikke sukkel</h1>"
 
-@app.route('/convert', methods=['GET'])
-def return_contacts():
-    contacts = get_contacts()
-    return contacts
+@app.route('/contacts', methods=['GET'])
+def contact_data():
+    fields = request.args.get('fields', 'all_fields')
+    data = extract_data.get_contacts(fields=fields)
+    return data
 
 if __name__ == '__main__':
     app.run()
--- extract_data.py
+++ extract_data.py
@@ -1,15 +1,41 @@
 import requests
 from authorizing import generate_authorization
 import os
+import pandas as pd
 
-def get_contacts():
+def get_contacts(fields='all_fields'):
 	authorization = generate_authorization()
 	params={'limit': 4000}
 
-	URL = "https://api.tomtoday.com/api/contacts/categories/1/contacts"
+	URL = 'https://api.tomtoday.com/api/contacts/categories/1/contacts'
 
 	response = requests.get(URL, headers=authorization, params=params)
 
 	response_data = response.json()['data']
 
-	return response_data
+	contacts = pd.json_normalize(response_data)
+
+	if fields == 'all_fields':
+		return_data = contacts.to_csv()
+
+	else:
+		fields_list = fields.split(',')
+
+		try: 
+			return_data =  contacts[fields_list].to_csv()
+		except KeyError:
+			return_data = "Error: dat veld bestaat niet in contacten"
+
+	return return_data
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+	print(get_contacts()[['label', 'email']].to_csv())
